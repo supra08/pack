4f621ca5b76af90d9d472ef0da981d9869c85d39
Micah Young <ymicah@vmware.com>
2020-03-12 10:56:00 -0400

    WIP
    Signed-off-by: Micah Young <ymicah@vmware.com>

--- internal/layer/windows_writer.go
+++ internal/layer/windows_writer.go
@@ -3,6 +3,8 @@ package layer
 import (
 	"archive/tar"
 	"io"
+	"path"
+	"strings"
 )
 
 type WindowsWriter struct {
@@ -14,13 +16,65 @@ func NewWindowsWriter(dataWriter io.WriteCloser) *WindowsWriter {
 }
 
 func (w WindowsWriter) Write(content []byte) (int, error) {
-	return -1, nil
+	return w.tarWriter.Write(content)
 }
 
 func (w WindowsWriter) WriteHeader(header *tar.Header) error {
+	if err := w.initializeLayer(); err != nil {
+		return err
+	}
+
+	err := w.writeParentPaths(header.Name)
+	if err != nil {
+		return err
+	}
+
+	header.Name = layerFilesPath(header.Name)
+
+	return w.tarWriter.WriteHeader(header)
+}
+
+func (w WindowsWriter) writeParentPaths(childPath string) error {
+	parentDir := path.Dir(childPath)
+	shallowestDir := ""
+	for _, pathPart := range strings.Split(parentDir, "/") {
+		shallowestDir = path.Join(shallowestDir, pathPart)
+
+		// skip root, already initialized
+		if shallowestDir == "" {
+			continue
+		}
+
+		if err := w.tarWriter.WriteHeader(&tar.Header{
+			Name:     layerFilesPath(shallowestDir),
+			Typeflag: tar.TypeDir,
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func layerFilesPath(origPath string) string {
+	return path.Join("Files", origPath)
+}
+
+func (w WindowsWriter) initializeLayer() error {
+	if err := w.tarWriter.WriteHeader(&tar.Header{
+		Name:     "Files",
+		Typeflag: tar.TypeDir,
+	}); err != nil {
+		return err
+	}
+	if err := w.tarWriter.WriteHeader(&tar.Header{
+		Name:     "Hives",
+		Typeflag: tar.TypeDir,
+	}); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (w WindowsWriter) Close() error {
-	return nil
+	return w.tarWriter.Close()
 }
--- internal/layer/windows_writer_test.go
+++ internal/layer/windows_writer_test.go
@@ -1,6 +1,12 @@
 package layer_test
 
 import (
+	"archive/tar"
+	"github.com/buildpacks/pack/internal/layer"
+	h "github.com/buildpacks/pack/testhelpers"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/sclevine/spec"
@@ -12,13 +18,79 @@ func TestWindowsWriter(t *testing.T) {
 }
 
 func testWindowsWriter(t *testing.T, when spec.G, it spec.S) {
-	when("#Write", func() {
-		it("does", func() {
+	when("#WriteHeader", func() {
+		it("writes with parent directoriess", func() {
+			var err error
+
+			f, err := ioutil.TempFile("", "windows-writer.tar")
+			h.AssertNil(t, err)
+			defer func() { f.Close(); os.Remove(f.Name()) }()
+
+			lw := layer.NewWindowsWriter(f)
+
+			err = lw.WriteHeader(&tar.Header{
+				Name:     "/cnb/lifecycle/my-file",
+				Typeflag: tar.TypeReg,
+			})
+			h.AssertNil(t, err)
+
+			err = lw.Close()
+			h.AssertNil(t, err)
+
+			f.Seek(0, 0)
+			tr := tar.NewReader(f)
+
+			th, _ := tr.Next()
+			h.AssertEq(t, th.Name, "Files")
+
+			th, _ = tr.Next()
+			h.AssertEq(t, th.Name, "Hives")
+
+			th, _ = tr.Next()
+			h.AssertEq(t, th.Name, "Files/cnb")
+
+			th, _ = tr.Next()
+			h.AssertEq(t, th.Name, "Files/cnb/lifecycle")
+
+			th, _ = tr.Next()
+			h.AssertEq(t, th.Name, "Files/cnb/lifecycle/my-file")
+			h.AssertEq(t, th.Typeflag, uint8(tar.TypeReg))
+
+			_, err = tr.Next()
+			h.AssertEq(t, err, io.EOF)
 		})
 	})
 
-	when("#WriteHeader", func() {
-		it("does", func() {
+	when("#Close", func() {
+		it("writes required parent dirs on empty image", func() {
+			var err error
+
+			f, err := ioutil.TempFile("", "windows-writer.tar")
+			h.AssertNil(t, err)
+			defer func() { f.Close(); os.Remove(f.Name()) }()
+
+			lw := layer.NewWindowsWriter(f)
+
+			err = lw.WriteHeader(&tar.Header{
+				Name:     "/cnb/lifecycle/my-file",
+				Typeflag: tar.TypeReg,
+			})
+			h.AssertNil(t, err)
+
+			err = lw.Close()
+			h.AssertNil(t, err)
+
+			f.Seek(0, 0)
+			tr := tar.NewReader(f)
+
+			th, _ := tr.Next()
+			h.AssertEq(t, th.Name, "Files")
+
+			th, _ = tr.Next()
+			h.AssertEq(t, th.Name, "Hives")
+
+			_, err = tr.Next()
+			h.AssertTrue(t, err, io.EOF)
 		})
 	})
 }
